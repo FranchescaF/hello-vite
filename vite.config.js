400a3881d6cd088c8796b8e85855035280141dd8
Franchesca Fatima Zelaya Contreras <fatimapolgar2001@gmail.com>
2025-03-16 20:09:58 -0500

    save

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 // vite.config.js
 
 import { defineConfig } from "vite";
-import postcss from "vite-plugin-postcss";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), postcss({ configFilePath: "./my-postcss-config.js" })],
+  plugins: [react()],
   server: {     
     port: 3000, // Cambia el número de puerto a 3000.
   },
